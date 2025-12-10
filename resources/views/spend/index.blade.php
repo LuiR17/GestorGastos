8677b45fc9654739a39f4688769cbb675dbd40db
LuiR17 <luiseduardoromero2002@gmail.com>
2025-12-10 15:33:21 -0500

    LogOut add

--- resources/views/spend/index.blade.php
+++ resources/views/spend/index.blade.php
@@ -8,6 +8,15 @@
 
         </ul>
         <a href="">Cuenta</a>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <x-dropdown-link :href="route('logout')"
+                onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                {{ __('Log Out') }}
+            </x-dropdown-link>
+        </form>
     </nav>
 
     <hr class="border-black mt-6">
